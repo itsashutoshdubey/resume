35c97130f30f7915feed351755d5da3931c9879d
Ashutosh Dubey <itsashutoshdubey@gmail.com>
2024-07-23 21:12:45 +0530

    Built web
    Branch: master
    Commit: 96ef6383f5c04e719a96e0fde14477defbac3ecd (dirty)
    
    package:peanut 5.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1519546481"
+    serviceWorkerVersion: "1548222601"
   }
 });
